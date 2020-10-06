6111abe2c89330f7b11c875cca121188986ef202
unknown <junior.silva_costa@live.com>
2020-10-05 21:02:50 -0300

    inciando java

---
+++ java/aulas/src/programa.java
@@ -0,0 +1,13 @@
+
+public class programa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int valor = 10;
+		System.out.println("Hello World !");
+		System.out.println("Até amanhã!");
+		System.out.println(valor);
+	}
+
+}
